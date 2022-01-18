602af58a2275a7cf92c78fa30a651b3f6b41fb95
Amr-Reda <amrreda547@gmail.com>
2022-01-18 23:07:14 +0200

    add client boilerplate

--- calculator_client/client.go
+++ calculator_client/client.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/Amr-Reda/calculator/calculator_proto"
+	"google.golang.org/grpc"
+)
 
 func main() {
 	fmt.Println("hello world")
+
+	conn, err := grpc.Dial("0.0.0.0:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Failed to establish connection: %v", err)
+	}
+	defer conn.Close()
+
+	client := calculator_proto.NewCalculatorClient(conn)
+	fmt.Printf("Created client: %f", client)
 }
