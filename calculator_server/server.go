2683ae4312a0f4b29e45204eec1abe75c08f4103
Amr-Reda <amrreda547@gmail.com>
2022-02-15 23:26:32 +0200

    add PrimeNumberDecomposition server side

--- calculator_server/server.go
+++ calculator_server/server.go
@@ -13,11 +13,30 @@ import (
 type server struct{}
 
 func (*server) Sum(ctx context.Context, req *calculator_proto.SumRequest) (*calculator_proto.SumResponse, error) {
+	fmt.Printf("Recieved Sum RPC\n")
 	result := req.GetNum1() + req.GetNum2()
 	res := &calculator_proto.SumResponse{
 		Result: result,
 	}
 	return res, nil
+}
+
+func (*server) PrimeNumberDecomposition(req *calculator_proto.PrimeNumberDecompositionRequest, stream calculator_proto.CalculatorService_PrimeNumberDecompositionServer) error {
+	fmt.Println("Recieved PrimeNumberDecomposition RPC")
+	num := req.GetNum()
+	divisor := int64(2)
+	for num > 1 {
+		if num%divisor == 0 {
+			stream.Send(&calculator_proto.PrimeNumberDecompositionResponse{
+				PrimeFactorResult: divisor,
+			})
+			num = num / divisor
+		} else {
+			divisor++
+			fmt.Printf("Divisor has increased to %v\n", divisor)
+		}
+	}
+	return nil
 }
 
 func main() {
